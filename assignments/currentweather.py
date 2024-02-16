4230061489045388c5f9675280f7abc8d8bed293
kevinsenan <kevinsenan@hotmail.com>
2024-02-16 15:01:21 +0000

    added code for temperature and windspeed

--- assignments/currentweather.py
+++ assignments/currentweather.py
@@ -2,3 +2,21 @@
 #
 #
 
+import requests
+import json
+# code for temperature
+url = "https://api.open-meteo.com/v1/forecast?latitude=52.6446122&longitude=-9.481083&current=temperature_2m"
+response = requests.get(url) 
+data = response.json()
+current_units = data["current"]
+temperature = current_units["temperature_2m"]
+print (f' The temperature is: {temperature} degrees C')
+
+
+ # code for wind speed
+url = "https://api.open-meteo.com/v1/forecast?latitude=52.6446122&longitude=-9.481083&current=wind_speed_10m"
+response = requests.get(url) 
+data = response.json()
+current_units = data["current"]
+windspeed = current_units["wind_speed_10m"]
+print (f' The wind speed is: {windspeed} km/h')
